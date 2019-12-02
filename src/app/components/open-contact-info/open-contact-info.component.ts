b8440f05fe5dd38595e85330718d6bd04866dd9b
Alexandr Makivchuk <alexmakivchuk@gmail.com>
2019-12-02 18:20:36 +0200

    bug fix

--- src/app/components/open-contact-info/open-contact-info.component.ts
+++ src/app/components/open-contact-info/open-contact-info.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {ContactStoreService} from '../../shared/services/contact-store.service';
 import {Router} from "@angular/router";
 
